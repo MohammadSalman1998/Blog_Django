9343b8f81612c921a8457ab482852ecf71ec3000
MohammadSalman1998 <mohammad.salman.m1998@gmail.com>
2024-05-01 10:46:12 +0300

    f

--- blog/myBlog/urls.py
+++ blog/myBlog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'myBlog'
+
 urlpatterns = [
-    path('', views.home),
+    path('', views.post_list, name= 'post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
 
 ]
--- blog/myBlog/views.py
+++ blog/myBlog/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Post
+from django.http import Http404
 # from django.http import HttpResponse
 
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'home.html')
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'myBlog/post/list.html',{'posts': posts})  
+
+
+def post_detail(request,id):
+    # method 1
+    # try:
+    #     post = Post.objects.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("Post does not exist")
+    # return render(request, 'myBlog/post/detail.html',{'post': post})
+
+    # method 2
+    post = get_object_or_404(Post, id = id , status = Post.Status.PUBLISHED)
+    return render(request, 'myBlog/post/detail.html',{'posts': post})
+
+
+# def home(request):
+#     return render(request, 'home.html')
